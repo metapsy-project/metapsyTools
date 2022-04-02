d115e6a87d17d4dc5b313da74504125d5993f3b3
Mathias Harrer <mathiasharrer@macbook-pro-6.localdomain>
2022-04-02 15:10:54 +0200

    update

---
+++ docs/assets/extra_script.js
@@ -0,0 +1,14 @@
+const footerRight = document.getElementsByClassName('pkgdown-footer-right')[0];
+
+var imgTUM = document.createElement('img');
+imgTUM.src = 'assets/figures/tum.png';
+imgTUM.width = "100";
+imgTUM.style = "Padding: 20px 20px 20px 20px;";
+
+var imgVU = document.createElement('img');
+imgVU.src = 'assets/figures/vu.png';
+imgVU.width = "125";
+imgVU.style = "Padding: 20px 20px 20px 20px;";
+
+footerRight.appendChild(imgTUM);
+footerRight.appendChild(imgVU);
