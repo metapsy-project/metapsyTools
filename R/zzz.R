46d68295dca447b19c821c5c275e59e39ac1917a
Mathias Harrer <mathiasharrer@macbook-pro-6.localdomain>
2022-04-02 15:28:02 +0200

    update

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,10 @@
                         crayon::cyan$bold("{metapsyTools}"),
                         crayon::cyan(" 0.3.1 [BETA]. \n \u2192 For help, run "),
                         crayon::green("vignette('metapsyTools')"),
-                        crayon::cyan(" in the R console."))
+                        crayon::cyan(" in the R console."),
+                        crayon::cyan("\n \u2192 Package documentation: "),
+                        crayon::green("metapsytools.protectlab.org"),
+                        crayon::cyan(".\n \u2192 Password: metapsyTools123!"))
 }
 
 
